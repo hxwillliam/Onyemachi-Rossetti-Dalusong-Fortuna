4505981aecddc7994fab39701e91d96589dc63dd
hxwilliam <159127557+hxwillliam@users.noreply.github.com>
2025-03-20 15:55:33 +0100

    test calcolo totale iva

--- tests/test.ts
+++ tests/test.ts
@@ -9,9 +9,9 @@ describe('Calcoli totale', () => {
         expect(totaleImponibile(carrello[]).toBe(145));
     };
 
-    // it('calcolare iva', () => { 
-    //     expect(totaleIva(carrello[]).toBe(29));
-    // };
+    it('calcolare iva', () => { 
+        expect(totaleIva(carrello[]).toBe(29));
+    };
 
     // it('calcolare totale ordine + iva', () => {
     //     expect(totaleOrdine(totaleImponibil() + totaleIva() ).toBe(174));
