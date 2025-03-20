0a58c5cc9640d7fd73ce976c8c3e2a817b5908e7
hxwilliam <159127557+hxwillliam@users.noreply.github.com>
2025-03-20 15:55:53 +0100

    test calcolo ordine

--- tests/test.ts
+++ tests/test.ts
@@ -13,9 +13,9 @@ describe('Calcoli totale', () => {
         expect(totaleIva(carrello[]).toBe(29));
     };
 
-    // it('calcolare totale ordine + iva', () => {
-    //     expect(totaleOrdine(totaleImponibil() + totaleIva() ).toBe(174));
-    // };
+    it('calcolare totale ordine + iva', () => {
+        expect(totaleOrdine(totaleImponibil() + totaleIva() ).toBe(174));
+    };
 
     // it('calcolare costo totale', () => {
     //     expect(totaleGenerale(totaleOrdine() + costoSpedizione)).toBe(174));
